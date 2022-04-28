06603ad0cc926ff124ec2dbe4bfa2f550fcfb820
chengyunlai <1257997441@qq.com>
2022-04-28 15:56:55 +0800

    Initial commit

---
+++ vite-project/src/components/NaiveButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <n-button>Default</n-button>
+  <n-button type="tertiary">
+    Tertiary
+  </n-button>
+  <n-button type="primary">
+    Primary
+  </n-button>
+  <n-button type="info">
+    Info
+  </n-button>
+  <n-button type="success">
+    Success
+  </n-button>
+  <n-button type="warning">
+    Warning
+  </n-button>
+  <n-button type="error">
+    Error
+  </n-button>
+</template>
+
+<script>
+export default {
+  name: "Naive-Button"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vite-project/src/views/NaiveWelcomeLayout.vue
@@ -0,0 +1,59 @@
+<template>
+  <n-layout>
+    <n-layout-header bordered>
+      <n-grid style="height: 64px" x-gap="12" :cols="4">
+        <n-gi>
+          <!--logo-->
+          <div class="light-green">
+            <n-image
+                width="64"
+                src="img/logo-img.png"
+                preview-disabled
+            />
+          </div>
+        </n-gi>
+        <n-gi>
+          <div class="green">
+            这里放跳转页面
+          </div>
+        </n-gi>
+        <n-gi>
+          <div class="light-green">
+            这里放登录和注册
+          </div>
+        </n-gi>
+        <n-gi>
+          <div class="green">
+            这里放GitHub/语雀的链接
+          </div>
+        </n-gi>
+      </n-grid>
+    </n-layout-header>
+    <n-layout has-sider>
+      <n-layout-sider bordered content-style="padding: 24px;">
+        海淀桥
+      </n-layout-sider>
+      <n-layout-content content-style="padding: 24px;">
+        <NaiveButton></NaiveButton>
+      </n-layout-content>
+    </n-layout>
+    <n-layout-footer bordered>成府路</n-layout-footer>
+  </n-layout>
+</template>
+
+<script>
+export default {
+  name: "NaiveLayout"
+}
+</script>
+
+<style scoped>
+.green{
+  background-color: aquamarine;
+}
+
+.light-green{
+  /*background-color: cadetblue;*/
+}
+
+</style>
